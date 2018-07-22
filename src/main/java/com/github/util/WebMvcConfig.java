4229031e083e27b50189b9c8bc1b6dc226591ea4
Baby <154518484@qq.com>
2018-07-22 23:19:31 +0800

    optimise.

--- src/main/java/com/github/util/WebMvcConfig.java
+++ src/main/java/com/github/util/WebMvcConfig.java
@@ -1,15 +1,12 @@
 package com.github.util;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @Configuration
-@ConditionalOnClass({FeignClient.class})
 public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Bean
@@ -17,12 +14,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		return new WebMvcRegistrations() {
 			@Override
 			public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
-				return new FeignRequestMappingHandlerMapping();
+				return new EnhanceRequestMappingHandlerMapping();
 			}
 		};
 	}
 
-	private static final class FeignRequestMappingHandlerMapping extends RequestMappingHandlerMapping {
+	private static final class EnhanceRequestMappingHandlerMapping extends RequestMappingHandlerMapping {
 		@Override
 		protected boolean isHandler(Class<?> beanType) {
 			return super.isHandler(beanType) && beanType.getPackage().getName().contains("controller");
