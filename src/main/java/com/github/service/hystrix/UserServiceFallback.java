47f1f8fcf63f89c1e025cf439bea669c00efd917
姚康明 <154518484@qq.com>
2018-07-23 11:31:55 +0800

    optimise.

--- src/main/java/com/github/service/hystrix/UserServiceFallback.java
+++ src/main/java/com/github/service/hystrix/UserServiceFallback.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserServiceFallback implements UserService {
 
-
 	@Override
 	public String hello() {
 		return "Hello Hystrix Fallback.";
